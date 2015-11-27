76548b4f729363e5ce1890146e31fdf33b088cf3
unilogue <simsluva@gmail.com>
2015-11-27 18:45:03 -0500

    Delete index.js

--- javascripts/index.js
+++
@@ -1,7 +0,0 @@
-//Define first typing example:
-          new TypingText(document.getElementById("example1"));
-          //Type out examples:
-          TypingText.runAll();
-new TypingText(document.getElementById("cmd"));
-          //Type out examples:
-          TypingText.run();
