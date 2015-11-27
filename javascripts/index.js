c7053edffde5cf6a9caecdd233a179018b3a11cd
unilogue <simsluva@gmail.com>
2015-11-27 18:31:42 -0500

    Create index.js

---
+++ javascripts/index.js
@@ -0,0 +1,7 @@
+//Define first typing example:
+          new TypingText(document.getElementById("example1"));
+          //Type out examples:
+          TypingText.runAll();
+new TypingText(document.getElementById("cmd"));
+          //Type out examples:
+          TypingText.run();
