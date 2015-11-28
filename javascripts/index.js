b80b641d825de78f3c9773876f1508396f0dc803
unilogue <simsluva@gmail.com>
2015-11-28 16:31:36 -0500

    Create index.js

---
+++ javascripts/index.js
@@ -0,0 +1,13 @@
+ var div = $('#div1').hide();
+
+$('input').keydown(function() {
+
+   var value = this.value;
+
+   if ($('input').val() == 'map') {
+       div.slideDown();
+   } else {
+      div.slideUp();   
+   }
+
+});
