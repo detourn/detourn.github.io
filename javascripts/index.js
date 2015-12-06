1e7cc4c8075447e5d82cbb209e1b50b45c2cb5c4
unilogue <simsluva@gmail.com>
2015-12-05 19:18:45 -0500

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,16 +1,17 @@
+// Hide both <div> by default
+$('#div1').hide();
+$('#div2').hide();
 
-  
-  var div = $('#div1').hide();
-
+// Check on keydown
 $('input').keydown(function() {
-
-   var value = this.value;
-
-   if ($('input').val() == 'map') {
-       div.slideDown();
-   } else {
-      div.slideUp();   
-   }
-
+  if ($('input').val() == 'map') {  // If input value is div1
+       $('#div2').hide();
+       $('#div1').slideDown();
+  } else if ($('input').val() == 'derive') {  // If input value is div2
+       $('#div1').hide();
+       $('#div2').slideDown();
+  } else {  // If input value is not equal to div1 or div2, hide both
+      $('#div1').hide();
+      $('#div2').hide();
+  }
 });
-  
