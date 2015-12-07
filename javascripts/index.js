c47f8f3796d41d6e06d8a60b9960f88a430c7dad
unilogue <simsluva@gmail.com>
2015-12-06 23:35:43 -0500

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,14 +9,17 @@ $('input').keydown(function() {
   if ($('input').val() == 'map') {  // If input value is div1
        $('#div2').hide();
        $('#div3').hide();
+       $('#error').hide();
        $('#div1').slideDown();
   } else if ($('input').val() == 'derive') {  // If input value is div2
        $('#div1').hide();
        $('#div3').hide();
+       $('#error').hide();
        $('#div2').slideDown();
   } else if ($('input').val() == 'glossary') {  // If input value is div2
        $('#div1').hide();
        $('#div2').hide();
+       $('#error').hide();
        $('#div3').slideDown();
   } else {
        $('#div1').hide();
