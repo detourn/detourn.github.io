d6556a37a831a8142fc54cf00a8eea72d9f06b70
unilogue <simsluva@gmail.com>
2015-12-06 21:33:51 -0500

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,24 +1,25 @@
-$(function() {
-  $('#commands').hide();
-       $('#help').click(function() {
-           $('#commands').slideDown();
-           return false;
-       });        
-   });
 // Hide both <div> by default
 $('#div1').hide();
 $('#div2').hide();
+$('#div3').hide();
 
 // Check on keydown
 $('input').keydown(function() {
   if ($('input').val() == 'map') {  // If input value is div1
        $('#div2').hide();
+       $('#div3').hide();
        $('#div1').slideDown();
   } else if ($('input').val() == 'derive') {  // If input value is div2
        $('#div1').hide();
+       $('#div3').hide();
        $('#div2').slideDown();
+  } else if ($('input').val() == 'glossary') {  // If input value is div2
+       $('#div1').hide();
+       $('#div2').hide();
+       $('#div3').slideDown();
   } else {  // If input value is not equal to div1 or div2, hide both
       $('#div1').hide();
       $('#div2').hide();
+      $('#div3').hide();
   }
 });
