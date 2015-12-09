8153516ee37da91f5fefd5a1dd4aab5c9a7345ca
unilogue <simsluva@gmail.com>
2015-12-09 18:12:35 -0500

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -7,21 +7,21 @@ $('#error').hide();
 // Check on keydown
 $('input').keydown(function() {
     if ($('input').val() == 'map') {  // If input value is div1
+       $('#div1').slideDown();
        $('#div2').hide();
        $('#div3').hide();
        $('#error').hide();
-       $('#div1').slideDown();
   } else if ($('input').val() == 'derive') {  // If input value is div2
        $('#div1').hide();
+       $('#div2').slideDown();
        $('#div3').hide();
        $('#error').hide();
-       $('#div2').slideDown();
   } else if ($('input').val() == 'glossary') {  // If input value is div3
        $('#div1').hide();
        $('#div2').hide();
-       $('#error').hide();
        $('#div3').slideDown();
-  } else if (!$('input').val() == 'map' && !$('input').val() == 'derive' && !$('input').val() == 'glossary') {  // If input value is wrong
+       $('#error').hide();
+  } else if ($('input').val() == 'map') {  // If input value is wrong
        $('#div1').hide();
        $('#div2').hide();
        $('#div3').hide();
@@ -30,5 +30,6 @@ $('input').keydown(function() {
        $('#div1').hide();
        $('#div2').hide();
        $('#div3').hide();
+       $('#error').hide();
   }
 });
