a8336191a1e481ce4faddb0e07954ffd7a9d9e2f
unilogue <simsluva@gmail.com>
2015-12-12 00:22:34 -0500

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,34 +1,28 @@
-// Hide both <div> by default
-$('#div1').hide();
-$('#div2').hide();
-$('#div3').hide();
-$('#error').hide();
+    $(document).ready(function() {
 
-// Check on keydown
-$('input').keydown(function() {
-    if ($('input').val() == 'map') {  // If input value is div1
-       $('#div1').slideDown();
-       $('#div2').hide();
-       $('#div3').hide();
-       $('#error').hide();
-  } else if ($('input').val() == 'derive') {  // If input value is div2
-       $('#div1').hide();
-       $('#div2').slideDown();
-       $('#div3').hide();
-       $('#error').hide();
-  } else if ($('input').val() == 'glossary') {  // If input value is div3
-       $('#div1').hide();
-       $('#div2').hide();
-       $('#div3').slideDown();
-  } else if ($('input').val() == '// wrong input //') {  // If input value is wrong
-       $('#div1').hide();
-       $('#div2').hide();
-       $('#div3').hide();
-       $('#error').slideDown();
-  } else {
-       $('#div1').hide();
-       $('#div2').hide();
-       $('#div3').hide();
-       $('#error').hide();
-  }
-});
+      // Hide both <div> by default
+      $('#div1').hide();
+      $('#div2').hide();
+      $('#div3').hide();
+      $('#error').hide();
+
+      // Check on keydown
+      $('input').keyup(function (e) {
+    		if (e.keyCode == 13) {
+          var value = $(this).val();
+          $('#div1').hide();
+          $('#div2').hide();
+          $('#div3').hide();
+          $('#error').hide();
+          if (value == 'map') { // If input value is div1
+            $('#div1').slideDown();
+          } else if (value == 'derive') { // If input value is div2
+            $('#div2').slideDown();
+          } else if (value == 'glossary') { // If input value is div3
+            $('#div3').slideDown();
+          } else if (value != '') { // If input value is wrong
+            $('#error').show();
+          }
+        }
+      });
+    });
