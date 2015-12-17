b477c2bd1f9877e7f0351e6a18052c309e45a36a
unilogue <simsluva@gmail.com>
2015-12-16 19:30:29 -0500

    Update cmd.js

--- javascripts/cmd.js
+++ javascripts/cmd.js
@@ -1,28 +1,28 @@
 jQuery(function($, undefined) {
 		
-	    $('body').terminal({
-			glossary: function() {
-				window.open('http://viperslang.tumblr.com/post/63454146164');
+	    $('#cmd').terminal({
+			g: function() {
+				this.echo('\b\t\t\t\t\t\t\t\t\t\t\t\t[[ ]]\t -\t\tthis city is a coded spine\t\t\b')
 			 },
         
-        map: function() {
-         this.echo('<p><small>Created with <a href="https://github.com/unilogue/datamaps" target="_blank">datamaps</a></small><br><br><b>A majority of Vice\'s viewers are from core countries, or the first world. The transparent countries mostly consist of peripheral countries, or the third world. Lighter shades indicate more viewership while darker indicates less. The U.S.A. makes up the most at 36.2%.</b></p>', {raw:true});
+        m: function() {
+         function load_home(){
+document.getElementById("content").innerHTML='<object type="text/html" data="termlib/tests/test1.txt" ></object>';
+}
 			 },
 		
 		
-			github: function() {
+			git: function() {
 				window.open('https://github.com/unilogue','_blank');
 			},
 		
-		    help: function() {
-		        this.echo('commands:');
-		        this.echo('\tmap...................................research paper');
-		        this.echo('\tderive................................digital personae');
-		        this.echo('\tglossary..............................a cyber love poem');
-			this.echo('\tclear.................................clears the screen');
-			this.echo('\tgithub................................my github profile');
-		        this.echo('\thelp..................................this help screen');
+		    '?': function() {
+            this.echo('COMMANDS: [m]ap\t\t[d]erive\t[g]lossary\r\n\t\t  [?] help\t [c]lose\t [git]hub');
 		    },
+        c: function() {
+            close();
+		    },
+        
 		    
 		    terminal_color: function(a) {
 				if (a == 'normal') {
@@ -32,9 +32,8 @@ jQuery(function($, undefined) {
 		        }
 		    },
 	    }, {
-	        greetings: 'type `help` for a list of commands.\r\n',
+	        greetings: 'COMMANDS: [m]ap\t\t[d]erive\t[g]lossary\r\n\t\t  [?] help\t [c]lose\t [git]hub',
 	        name: 'unilogue',
-	        height: window.innerHeight - 36,
 	        prompt: '> '
 		});
 	});
