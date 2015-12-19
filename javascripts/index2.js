5b4a0f548ef982598308383033703426e1882b0a
unilogue <simsluva@gmail.com>
2015-12-19 17:56:15 -0500

    Update index2.js

--- javascripts/index2.js
+++ javascripts/index2.js
@@ -18,14 +18,14 @@ $(document).ready(function() {
           $('#error').hide();
           if (value == 'map') { // If input value is div1
             $('#div1').show();
-            $('#div1').load("https://unilogue.github.io/map");
+            $('#div1').load("https://unilogue.github.io/commands/map");
             $.getScript("https://unilogue.github.io/javascripts/map1.js");
           } else if (value == 'derive') { // If input value is div2
             $('#div2').show();
-            $('#div2').load("https://unilogue.github.io/derive");
+            $('#div2').load("https://unilogue.github.io/commands/derive");
           } else if (value == 'glossary') { // If input value is div3
             $('#div3').show();
-            $('#div3').load("https://unilogue.github.io/glossary");
+            $('#div3').load("https://unilogue.github.io/commands/glossary");
           } else if (value != '') { // If input value is wrong
             $('#error').show();
           }
