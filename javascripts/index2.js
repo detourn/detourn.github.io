427b3ad7d2f5f518f1b0510505c3e5d7684f2ddc
unilogue <simsluva@gmail.com>
2015-12-18 20:55:44 -0500

    Update index2.js

--- javascripts/index2.js
+++ javascripts/index2.js
@@ -1,19 +1,28 @@
  $(document).ready(function() {
 
       // Hide both <div> by default
+      $('#div1').hide();
+      $('#div2').hide();
+      $('#div3').hide();
       $('#error').hide();
 
       // Check on keydown
       $('input').keyup(function (e) {
     		if (e.keyCode == 13) {
           var value = $(this).val();
+          $('#div1').hide();
+          $('#div2').hide();
+          $('#div3').hide();
           $('#error').hide();
           if (value == 'map') { // If input value is div1
+            $('#div1').show();
             $('#div1').load("https://unilogue.github.io/commands #div1");
-            $('#div1').getScript("https://unilogue.github.io/javascripts/map1.js");
+            $('#world').getScript("https://unilogue.github.io/javascripts/map1.js");
           } else if (value == 'derive') { // If input value is div2
+            $('#div2').show();
             $('#div2').load("https://unilogue.github.io/commands #div2");
           } else if (value == 'glossary') { // If input value is div3
+            $('#div3').show();
             $('#div3').load("https://unilogue.github.io/commands #div3");
           } else if (value != '') { // If input value is wrong
             $('#error').show();
