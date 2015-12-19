8b2b6f3a054d18e4d3f78790f5c8327c5e05755a
unilogue <simsluva@gmail.com>
2015-12-18 19:42:46 -0500

    Create index2.js

---
+++ javascripts/index2.js
@@ -0,0 +1,30 @@
+ $(document).ready(function() {
+
+      // Hide both <div> by default
+      $('#div1').hide();
+      $('#div2').hide();
+      $('#div3').hide();
+      $('#error').hide();
+
+      // Check on keydown
+      $('input').keyup(function (e) {
+    		if (e.keyCode == 13) {
+          var value = $(this).val();
+          $('#div1').hide();
+          $('#div2').hide();
+          $('#div3').hide();
+          $('#error').hide();
+          if (value == 'map') { // If input value is div1
+            $('#div1').load("https://unilogue.github.io/commands #div1");
+          } else if (value == 'derive') { // If input value is div2
+            $('#div2').load("https://unilogue.github.io/commands #div2");
+          } else if (value == 'glossary') { // If input value is div3
+            $('#div3').load("https://unilogue.github.io/commands #div3");
+          } else if (value != '') { // If input value is wrong
+            $('#error').show();
+          }
+        }
+      });
+    });
+
+term.echo ('<div id="map"></div>', {raw:true});    $('#map').load("https://unilogue.github.io/commands #div1");
